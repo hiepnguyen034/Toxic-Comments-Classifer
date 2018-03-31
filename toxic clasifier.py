87867d4f4286a7461835e572bd4524d322de1058
hiepnguyen034 <hiepnguyen034@gmail.com>
2018-03-31 15:42:09 -0400

    Update toxic clasifier.py

--- toxic clasifier.py
+++ toxic clasifier.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
-from sklearn import svm
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
